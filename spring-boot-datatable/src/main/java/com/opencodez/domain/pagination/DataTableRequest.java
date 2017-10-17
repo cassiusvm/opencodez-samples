b564a4b549447fd00dd971276b62d595cdcb1a92
Cassius Vinicius de Magalhães <cviniciusm@gmail.com>
2017-10-17 19:10:03 -0200

    Refatorações

--- spring-boot-datatable/src/main/java/com/opencodez/domain/pagination/DataTableRequest.java
+++ spring-boot-datatable/src/main/java/com/opencodez/domain/pagination/DataTableRequest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.opencodez.domain.pagination;
 
 import java.util.ArrayList;
@@ -13,216 +10,35 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.opencodez.util.AppUtil;
 
-/**
- * The Class DataTableRequest.
- *
- * @author pavan.solapure
- */
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DataTableRequest<T> {
 	
-	/** The unique id. */
 	private String uniqueId;
 	
-	/** The draw. */
 	private String draw;
 	
-	/** The start. */
 	private Integer start;
 	
-	/** The length. */
 	private Integer length;
 	
-	/** The search. */
 	private String search;
 	
-	/** The regex. */
 	private boolean regex;
 
-	/** The columns. */
 	private List<DataTableColumnSpecs> columns;
 	
-	/** The order. */
 	private DataTableColumnSpecs order;
 	
-	/** The is global search. */
 	private boolean isGlobalSearch;
 
-	/**
-	 * Instantiates a new data table request.
-	 *
-	 * @param request the request
-	 */
 	public DataTableRequest(HttpServletRequest request) {
 		prepareDataTableRequest(request);
 	}
-	
-	/**
-	 * Gets the unique id.
-	 *
-	 * @return the uniqueId
-	 */
-	public String getUniqueId() {
-		return uniqueId;
-	}
 
-	/**
-	 * Sets the unique id.
-	 *
-	 * @param uniqueId the uniqueId to set
-	 */
-	public void setUniqueId(String uniqueId) {
-		this.uniqueId = uniqueId;
-	}
-
-	/**
-	 * Gets the start.
-	 *
-	 * @return the start
-	 */
-	public Integer getStart() {
-		return start;
-	}
-
-	/**
-	 * Sets the start.
-	 *
-	 * @param start the start to set
-	 */
-	public void setStart(Integer start) {
-		this.start = start;
-	}
-
-	/**
-	 * Gets the length.
-	 *
-	 * @return the length
-	 */
-	public Integer getLength() {
-		return length;
-	}
-
-	/**
-	 * Sets the length.
-	 *
-	 * @param length the length to set
-	 */
-	public void setLength(Integer length) {
-		this.length = length;
-	}
-
-	/**
-	 * Gets the search.
-	 *
-	 * @return the search
-	 */
-	public String getSearch() {
-		return search;
-	}
-
-	/**
-	 * Sets the search.
-	 *
-	 * @param search the search to set
-	 */
-	public void setSearch(String search) {
-		this.search = search;
-	}
-
-	/**
-	 * Checks if is regex.
-	 *
-	 * @return the regex
-	 */
-	public boolean isRegex() {
-		return regex;
-	}
-
-	/**
-	 * Sets the regex.
-	 *
-	 * @param regex the regex to set
-	 */
-	public void setRegex(boolean regex) {
-		this.regex = regex;
-	}
-
-	/**
-	 * Gets the columns.
-	 *
-	 * @return the columns
-	 */
-	public List<DataTableColumnSpecs> getColumns() {
-		return columns;
-	}
-
-	/**
-	 * Sets the columns.
-	 *
-	 * @param columns the columns to set
-	 */
-	public void setColumns(List<DataTableColumnSpecs> columns) {
-		this.columns = columns;
-	}
-
-	/**
-	 * Gets the order.
-	 *
-	 * @return the order
-	 */
-	public DataTableColumnSpecs getOrder() {
-		return order;
-	}
-
-	/**
-	 * Sets the order.
-	 *
-	 * @param order the order to set
-	 */
-	public void setOrder(DataTableColumnSpecs order) {
-		this.order = order;
-	}
-	
-	/**
-	 * Gets the draw.
-	 *
-	 * @return the draw
-	 */
-	public String getDraw() {
-		return draw;
-	}
-
-	/**
-	 * Sets the draw.
-	 *
-	 * @param draw the draw to set
-	 */
-	public void setDraw(String draw) {
-		this.draw = draw;
-	}
-
-	/**
-	 * Checks if is global search.
-	 *
-	 * @return the isGlobalSearch
-	 */
-	public boolean isGlobalSearch() {
-		return isGlobalSearch;
-	}
-
-	/**
-	 * Sets the global search.
-	 *
-	 * @param isGlobalSearch the isGlobalSearch to set
-	 */
-	public void setGlobalSearch(boolean isGlobalSearch) {
-		this.isGlobalSearch = isGlobalSearch;
-	}
-
-	/**
-	 * Prepare data table request.
-	 *
-	 * @param request the request
-	 */
 	private void prepareDataTableRequest(HttpServletRequest request) {
 		
 		Enumeration<String> parameterNames = request.getParameterNames();
@@ -284,11 +100,6 @@ public class DataTableRequest<T> {
 		return lstOfParams.size();
 	}
 	
-	/**
-	 * Gets the pagination request.
-	 *
-	 * @return the pagination request
-	 */
 	public PaginationCriteria getPaginationRequest() {
 		
 		PaginationCriteria pagination = new PaginationCriteria();
@@ -316,8 +127,7 @@ public class DataTableRequest<T> {
 		
 		return pagination;
 	}
-	
-	/** The max params to check. */
+
 	private int maxParamsToCheck = 0;
 	
 }
